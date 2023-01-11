ee78e24cd40ff9e1aba0608e8a3ebbc44b72cd98
karthikgoud <karthikrv09@gmail.com>
2023-01-11 12:54:56 +0530

    multiple card with manual data

--- App.js
+++ App.js
@@ -15,22 +15,63 @@ const Header = () => {
   );
 };
 
-const Card = () => {
+const restaurantList = [
+  {
+    id: 1,
+    image:
+      "https://b.zmtcdn.com/data/pictures/7/19672427/fab0f327a0193957ee122f8c72c4d2f9_featured_v2.jpg",
+    name: "Ironhill Bangalore",
+    rating: "4.5",
+    place: "Bangalore",
+  },
+  {
+    id: 2,
+    image:
+      "https://b.zmtcdn.com/data/pictures/0/18621880/995850eced07cd1783fab1b8e25de4ff_featured_v2.jpg",
+    name: "Foxtrot",
+    rating: "4.4",
+    place: "Bangalore",
+  },
+  {
+    id: 3,
+    image:
+      "https://b.zmtcdn.com/data/pictures/5/57795/ef1898f2ac7b29aabb4e3b80325b9118_featured_v2.jpg",
+    name: "Meghana Foods",
+    rating: "4.7",
+    place: "Bangalore",
+  },
+  {
+    id: 4,
+    image:
+      "https://b.zmtcdn.com/data/pictures/chains/4/50574/0e9b77dd56dc25acc2d4eb67473af959_featured_v2.jpg",
+    name: "KFC",
+    rating: "4.5",
+    place: "Bangalore",
+  },
+];
+
+const Card = (props) => {
+  //   console.log(props);
+  //destructuring
+  const { image, name, rating, place } = props.restautant;
   return (
     <div className="card-container">
-      <img src="https://b.zmtcdn.com/data/pictures/7/19672427/fab0f327a0193957ee122f8c72c4d2f9_featured_v2.jpg" />
-      <h2>Ironhill Bangalore</h2>
-      <p>4.5</p>
-      <p>Marathahalli,Bangalore</p>
+      <img src={image} />
+      <h2>{name}</h2>
+      <p>{rating}</p>
+      <p>{place}</p>
     </div>
   );
 };
 
 const Body = () => {
   return (
-    <>
-      <Card />
-    </>
+    <div className="cards-div">
+      <Card restautant={restaurantList[0]} />
+      <Card restautant={restaurantList[1]} />
+      <Card restautant={restaurantList[2]} />
+      <Card restautant={restaurantList[3]} />
+    </div>
   );
 };
 
